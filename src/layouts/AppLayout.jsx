e52d00b9e04abcc7764adf0046c880b3f64c1de0
Thies Feindt <thies@mac.speedport.ip>
2026-04-04 16:13:32 +0200

    Fix Sidebar import casing for Linux build

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom'
-import Sidebar from '../components/layout/Sidebar'
+import Sidebar from '../components/layout/sidebar'
 import Topbar from '../components/layout/Topbar'
 
 export default function AppLayout() {
