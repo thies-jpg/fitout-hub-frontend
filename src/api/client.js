1a805616c400ed65b19733951fbf4336a26ea569
Thies Feindt <thies@mac.speedport.ip>
2026-04-05 12:14:28 +0200

    Use VITE_API_BASE_URL for frontend API base

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://178.104.116.71:3001/api'
+const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://178.104.116.71:3001/api'
 
 async function handleResponse(response) {
   if (!response.ok) {
